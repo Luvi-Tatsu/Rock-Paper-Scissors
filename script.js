e37f5f6263c10529d11563b03d1965536f5310a6
Luvi-Tatsu <195727939+Luvi-Tatsu@user.noreply.github.com>
2025-03-04 22:12:28 +0800

    Mar-3-2025

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.log("Hello World");
-
+// we add a player Score variable
+let playerScore = 0;
+// we add a computer Score variable
+ let computerScore = 0;
 //this function declairs the computer move
 function getComputerChoice(){
     const picks = ["rock","paper","scissors"]
@@ -7,7 +10,7 @@ function getComputerChoice(){
     return picks [Math.floor(Math.random()*picks.length)];  
 }
 
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 // This function is for number 3, I put a function to promt a Players Choice
 function getPlayerChoice(){
 // Place is a variable for the prompt which the player will write to. its still not key insensitive
@@ -18,39 +21,41 @@ function getPlayerChoice(){
 //console.log(getPlayerChoice());
 // this is for a round for the game.
 function playRound(playerChoice, computerChoice) {
+    let playerWin ='You Win, Your Score: ';
+    let compWin = 'You lose! ${computerSelection} beats ${humanSelection} Computer Score: ';
     // if human and computer choices were the same, its a tie
     if (humanSelection === computerSelection){
         return 'tie';}  
     // if human is scissor and compter is paper human wins
     if (humanSelection === "scissors" && computerSelection === "paper"){
-            return 'you win',playerScore()+1;}
+            return  [playerWin, playerScore++] ;}
     // if human is rock and compter is scissors human wins       
     if (humanSelection === "rock" && computerSelection === "scissors"){
-            return 'you win',playerScore()+1;}
+            return  [playerWin, playerScore++] ;}
     // if human is paper and compter is rock human wins       
     if (humanSelection === "paper" && computerSelection === "rock"){
-            return 'you win',playerScore()+1;}
+            return  [playerWin, playerScore++] ;}
     // if its vice versa computer wins
     else {
-        return `You lose! ${computerSelection} beats ${humanSelection}`,computerScore()+1;
+            return  [compWin, computerScore++];
                 }
   }
+  //
   // we define our getPlayerChoice function as variable and make it all to lower case or key insensitive.
   const humanSelection = getPlayerChoice().toLowerCase();
   //we define our getComputerChoice funtion as variable note: im still confuse as to why we do it
   const computerSelection = getComputerChoice();
-  // we add a player Score variable
-  const playerScore = 0;
- // we add a computer Score variable
-  const computerScore = 0;
  
   
   playRound(humanSelection, computerSelection);
   console.log(playRound());
-
+  console.log(playerScore);
+  console.log(computerScore);
+  
 
 //   function playGame(){
-//     for (let i = 0; i < 5; i++) {
+//     if (playerScore && computerScore != 5){
+//        return playRound(humanSelection, computerSelection);
 //         console.log(playRound());
 //     }
 //   }
