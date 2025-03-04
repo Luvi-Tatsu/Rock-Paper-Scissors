0c0b39fcde71fd28c9f214f33a8f8e896a993bba
Luvi-Tatsu <195727939+Luvi-Tatsu@user.noreply.github.com>
2025-03-04 20:44:46 +0800

    rework code

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("Hello World");
+
+// const playerScore = 0;
+// const computerScore = 0;
+
+// function getComputerChoice(){
+//     const pick = ['rock', 'paper', 'scissors']
+// return pick [Math.floor (Math.random ()* pick.length)];
+// }
+
+// function getPlayerChoice(){
+//     place = prompt;
+//     return place("rock, paper or scissors");
+// }
+// // console.log(getPlayerChoice());
+
+// function playRound(getPlayerChoice, getComputerChoice) {
+//     if (humanSelection === computerSelection){
+//         return 'tie';}  
+
+//     if (humanSelection === "scissors" && computerSelection === "paper"){
+//             return 'you win';}
+           
+//     if (humanSelection === "rock" && computerSelection === "scissors"){
+//             return 'you win';}
+//     if (humanSelection === "paper" && computerSelection === "rock"){
+//             return 'you win';}
+//     else {
+//         return `You lose! ${computerSelection} beats ${humanSelection}`;
+//                 }
+//   }
+//   console.log(getComputerChoice());
+//   const humanSelection = getPlayerChoice().toLowerCase();
+//   const computerSelection = getComputerChoice();
+  
+//   playRound(humanSelection, computerSelection);
+
+//   function playGame(){
+//     for (let i = 0; i < 5; i++) {
+//         console.log(playRound());
+//     }
+//   }
+   
+
