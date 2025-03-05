1bb1b6ed852e4f8ba524712cddcd209455cf54a5
Luvi-Tatsu <195727939+Luvi-Tatsu@user.noreply.github.com>
2025-03-05 09:43:22 +0800

    Add score system

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Rock-Paper-Scissors
 
 Rock Paper Scissors activity
+NOTE: ANY OTHER PLAYER INPUT OTHER THAN "rock, paper, scissors" WILL RESULT IN COMPUTER WINNING
--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 console.log("Hello World");
-// we add a player Score variable
-let playerScore = 0;
-// we add a computer Score variable
- let computerScore = 0;
+
 //this function declairs the computer move
 function getComputerChoice(){
     const picks = ["rock","paper","scissors"]
@@ -20,45 +17,52 @@ function getPlayerChoice(){
 // console no longer needed but was helpfull
 //console.log(getPlayerChoice());
 // this is for a round for the game.
-function playRound(playerChoice, computerChoice) {
-    let playerWin ='You Win, Your Score: ';
-    let compWin = 'You lose! ${computerSelection} beats ${humanSelection} Computer Score: ';
+
+function playRound(playerChoice, computerChoice) { 
     // if human and computer choices were the same, its a tie
     if (humanSelection === computerSelection){
         return 'tie';}  
     // if human is scissor and compter is paper human wins
     if (humanSelection === "scissors" && computerSelection === "paper"){
-            return  [playerWin, playerScore++] ;}
+            return  'You Win';}
     // if human is rock and compter is scissors human wins       
     if (humanSelection === "rock" && computerSelection === "scissors"){
-            return  [playerWin, playerScore++] ;}
+            return  'You Win';}
     // if human is paper and compter is rock human wins       
     if (humanSelection === "paper" && computerSelection === "rock"){
-            return  [playerWin, playerScore++] ;}
+            return  'You Win';}
     // if its vice versa computer wins
     else {
-            return  [compWin, computerScore++];
+            return `You lose! ${computerSelection} beats ${humanSelection}`;
                 }
   }
-  //
+
   // we define our getPlayerChoice function as variable and make it all to lower case or key insensitive.
   const humanSelection = getPlayerChoice().toLowerCase();
   //we define our getComputerChoice funtion as variable note: im still confuse as to why we do it
   const computerSelection = getComputerChoice();
  
-  
   playRound(humanSelection, computerSelection);
   console.log(playRound());
-  console.log(playerScore);
-  console.log(computerScore);
+ 
+
+function gameScore(){
+    // we add a player Score variable
+ playerScore = 0;
+// we add a computer Score variable
+ computerScore = 0;
+ // tells that if playRound out put is you win score of player is = 1
+ if (playRound() === 'You Win' ){
+    return `Player: ${playerScore + 1}`;
+ }
+ // tells that if playRound out put is tie score is also tie
+ if (playRound() === 'tie')
+    return playerScore && computerScore + 0;
+ // tells that if playRound has other output then computer is +1
+// NOTE: ANY OTHER PLAYER INPUT OTHER THAN "rock, paper, scissors" WILL RESULT IN COMPUTER WINNING
+else{
+    return `Computer: ${computerScore + 1}`;
+}
+}
+console.log(gameScore());
   
-
-//   function playGame(){
-//     if (playerScore && computerScore != 5){
-//        return playRound(humanSelection, computerSelection);
-//         console.log(playRound());
-//     }
-//   }
-   
-
-
